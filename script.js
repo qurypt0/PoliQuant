769ba81b60806497c4bff794773af573c63c412e
Luke E <luke.endicott2009@outlook.com>
2025-02-20 08:42:39 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// This script will handle the "Subscribe Now" button click event.
+document.querySelector("button").addEventListener("click", function() {
+    alert("Thank you for subscribing to Poliquant!");
+});
